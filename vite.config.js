95d61300e4afd09b9499acfcdba46b4c070233ca
Max Collins Botchway <your-github-email@example.com>
2025-06-14 23:02:38 +0200

    Initial commit for FrameKit frontend

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './', // THIS FIXES asset path resolution on Netlify
   plugins: [react()],
 });
